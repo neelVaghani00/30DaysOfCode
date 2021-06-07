fb470489d1d6e15606865a683fefc2b2ebc14405
neelVaghani00 <neelvaghani50@gmail.com>
2021-06-07 08:51:23 +0530

    Day 3 complate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catlog/pages/home_page.dart';
+import 'package:flutter_catlog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,30 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Catalog App"),
-        ),
-        drawer: Drawer(
-          child: Text("Neel Vaghani"),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              
-              icon: Icon(Icons.home),
-              label: 'Home',
-              backgroundColor: Colors.red,
-              
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-              backgroundColor: Colors.red,
-            ),
-          ],
-        ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
       ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage()
+      },
     );
   }
 }
